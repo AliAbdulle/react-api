b88d0b46e9016f0cd2bec738029399c4023ec338
AliAbdulle <alidhobey@gmail.com>
2020-02-27 23:33:23 -0600

    Saved all files and push to git

--- src/components/News/NewSingle.js
+++ src/components/News/NewSingle.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const newSingle = ({item}) => (  
-      <ul>
-        <p>{item.title}</p>
-    </ul>
+    <div className='col s4'>
+        <div className="card">
+            <div className="card-image">
+                <img src={item.urlToImage} alt={item.title} />
+                <p className="card-title">{item.source.name}</p>
+            </div>
+            <div className="card-content">
+                <p>{item.title}</p>
+            </div>
+            <div className="card-action">
+                <a href={item.url} target="_blank">Full Article</a>
+            </div>
+        </div>
+    </div>
 )
 
 export default newSingle;
