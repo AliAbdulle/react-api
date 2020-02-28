fd37b23ca82a764c927e3c41532b4bb93094657a
AliAbdulle <alidhobey@gmail.com>
2020-02-27 23:05:19 -0600

    Added api to fetch the data

--- src/components/News/NewSingle.js
+++ src/components/News/NewSingle.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const newSingle = ({item}) => {
-    <ul>
+const newSingle = ({item}) => (  
+      <ul>
         <p>{item.title}</p>
     </ul>
-}
+)
 
 export default newSingle;
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import NewSingle from './NewsSingle';
+import NewSingle from './NewSingle';
 
 class News extends Component {
-    renderItems() {
-        return this.props.item.map((item) => {
-            <NewSingle key={item.id} item={item} />
+    constructor(props) {
+        super(props)
+        this.state ={
+            news: [],
+        }
+    }
+
+    componentWillMount() {
+        const url = 'http://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=11d2c66f6cf0407481155a121f0a198a';
+
+        fetch(url)
+        .then((response) => {
+            return response.json();
         })
+
+        .then((data) => {
+            this.setState({
+                news: data.articles
+            })
+        })
+        .catch((error) => console.log(error));
+    }
+
+    renderItems() {
+        return this.state.news.map((item) => (        
+                <NewSingle key={item.id} item={item} />
+        ))
     }
     render() {
         return (
