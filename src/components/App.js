4d10ad3b716d37cf6a4ea3b212c50d929b0e4bbc
AliAbdulle <alidhobey@gmail.com>
2020-03-02 22:22:14 -0600

    create news3 section

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,10 @@ class App extends Component {
       news2: {
         type: 'everything',
         query: 'dapple&from=2020-02-28&to=2020-02-28&sortBy=popularity'
+      },
+      news3: {
+        type: 'everything',
+        query: 'domains=wsj.com,nytimes.com'
       }
     };
   }
@@ -24,7 +28,7 @@ class App extends Component {
       <div className="container-fluid" >
         <div className="navbar-fix">
           <div className="nav-wrapper indigo lighten-4">
-            <a href="#" className="brain-logo center">My Feed</a>
+            <a href="/" className="brain-logo center">My Feed</a>
           </div>
         </div>
         <div className="row">
@@ -33,7 +37,7 @@ class App extends Component {
             <News news={this.state.new2} />
           </div>
           <div className="col s4">
-            <sidenews />
+            <sidenews news3={this.state.news3} />
           </div>
         </div>
       </div>
